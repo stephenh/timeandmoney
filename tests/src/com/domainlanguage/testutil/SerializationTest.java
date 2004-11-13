3d42926d5d5f6bbddf7876de4f184731e7dac1d1
alphaprojects <alphaprojects@a8ba37aa-5335-0410-8f99-849fd68a1e00>
2004-11-13 21:17:03 +0000

    Fixed serialization code so that it doesn't hang when an object we are trying to serialize doesn't meet serialization criteria. Now an exception is thrown instead.
    git-svn-id: https://timeandmoney.svn.sourceforge.net/svnroot/timeandmoney/timeandmoney/trunk@53 a8ba37aa-5335-0410-8f99-849fd68a1e00

--- tests/src/com/domainlanguage/testutil/SerializationTest.java
+++ tests/src/com/domainlanguage/testutil/SerializationTest.java
@@ -3,10 +3,12 @@
  */
 package com.domainlanguage.testutil;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.PipedInputStream;
-import java.io.PipedOutputStream;
 import java.io.Serializable;
 
 import junit.framework.AssertionFailedError;
@@ -18,19 +20,26 @@ import junit.framework.AssertionFailedError;
  */
 public class SerializationTest {
     
+    
+private static final String TEST_FILE = "test.txt";
     public static void assertSerializationWorks(Object toSerialize) throws AssertionFailedError {
         if( !(toSerialize instanceof Serializable) ) {
             throw new AssertionFailedError("Object doesn't implement java.io.Serializable interface");
         }
         
+        FileOutputStream fout = null;
+        FileInputStream fin = null;
+        File serFile = null;
         try {
-            PipedOutputStream pipedOut = new PipedOutputStream();
-            PipedInputStream pipedIn = new PipedInputStream(pipedOut);
+            serFile = new File(TEST_FILE);
+            fout = new FileOutputStream(serFile);
+            fin = new FileInputStream(serFile);
             
-            ObjectOutputStream out = new ObjectOutputStream(pipedOut);
+            ObjectOutputStream out = new ObjectOutputStream(fout);
             out.writeObject(toSerialize);
+            out.flush();
             
-            ObjectInputStream in = new ObjectInputStream(pipedIn);
+            ObjectInputStream in = new ObjectInputStream(fin);
             Object deserialized = in.readObject();
             
             if( !(toSerialize.equals(deserialized))) {
@@ -38,10 +47,24 @@ public class SerializationTest {
             }
         }
         catch (AssertionFailedError e) {
-        	throw e;
+            throw e;
         }
         catch (Exception e) {
             throw new AssertionFailedError("Exception while serializing: " + e);
+        }
+        finally {
+            try {
+                if( fout != null ) {
+                    fout.close();
+                }
+                if( fin != null ) {
+                    fin.close();
+                }
+                serFile.delete();
+            }
+            catch (IOException e1) {
+                // ignore
+            }
         }
     }
 
