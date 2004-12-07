f602318bbc0899e87aeb045cea6a9b759edd0fe2
sadehb <sadehb@a8ba37aa-5335-0410-8f99-849fd68a1e00>
2004-12-07 07:27:58 +0000

    *** empty log message ***
    git-svn-id: https://timeandmoney.svn.sourceforge.net/svnroot/timeandmoney/timeandmoney/trunk@86 a8ba37aa-5335-0410-8f99-849fd68a1e00

--- src/com/domainlanguage/time/TimeInterval.java
+++ src/com/domainlanguage/time/TimeInterval.java
@@ -62,6 +62,8 @@ public class TimeInterval extends ConcreteInterval {
 
 	public TimeInterval(TimePoint start, boolean startIncluded, TimePoint end, boolean endIncluded) {
 //		assert start.compareTo(end) < 0;  //This should really be an Interval invariant.
+//	revisit: also, as with any java assert, an AssertionError will be thrown only if compiled with the -ea option (enableassertions)
+//	so maybe we want to throw an IllegalArgumentException instead?		
 		super(start, startIncluded, end, endIncluded);
 	}
 
