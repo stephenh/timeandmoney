a14c2adc2b7a597d383415068d3f59f23ace4c3e
btbuxton <btbuxton@a8ba37aa-5335-0410-8f99-849fd68a1e00>
2005-09-26 01:50:55 +0000

    Changed FilteredTestCaseCollector to be able to look for test files inside of Jars. ClassPathTestCollector did not have this capability and unfortuantely, made some of its core methods private. SO, I had to copy some of the methods verbatim. Now, we can have our sanity check when we build our tests in the jar.
    git-svn-id: https://timeandmoney.svn.sourceforge.net/svnroot/timeandmoney/timeandmoney/trunk@122 a8ba37aa-5335-0410-8f99-849fd68a1e00

--- tests/src/com/domainlanguage/tests/FilteredTestCaseCollector.java
+++ tests/src/com/domainlanguage/tests/FilteredTestCaseCollector.java
@@ -6,8 +6,10 @@
 
 package com.domainlanguage.tests;
 
+import java.io.*;
 import java.lang.reflect.*;
 import java.util.*;
+import java.util.jar.*;
 
 import junit.framework.*;
 import junit.runner.*;
@@ -18,7 +20,9 @@ import com.domainlanguage.util.*;
  * I loads all TestCase subclasses on the class path, excluding specified
  * filters
  */
-public class FilteredTestCaseCollector extends ClassPathTestCollector {
+public class FilteredTestCaseCollector {
+    static final int SUFFIX_LENGTH= ".class".length();
+    
     private TestCaseClassLoader loader = new TestCaseClassLoader();
     private String[] exludedPackages = new String[0];
     private Set exludedClasses = new HashSet();
@@ -50,7 +54,8 @@ public class FilteredTestCaseCollector extends ClassPathTestCollector {
     private void reloadExcludedTypes() {
         try {
             for (int i = 0; i < exludedTypes.length; i++)
-                exludedTypes[i] = loader.loadClass(exludedTypes[i].getName(), false);
+                exludedTypes[i] = loader.loadClass(exludedTypes[i].getName(),
+                        false);
         } catch (ClassNotFoundException ignore) {
         }
     }
@@ -87,7 +92,90 @@ public class FilteredTestCaseCollector extends ClassPathTestCollector {
     }
 
     private boolean isTestClass(Class testClass) {
-        return !Modifier.isAbstract(testClass.getModifiers()) && Modifier.isPublic(testClass.getModifiers()) && TypeCheck.is(testClass, TestCase.class);
+        return !Modifier.isAbstract(testClass.getModifiers())
+                && Modifier.isPublic(testClass.getModifiers())
+                && TypeCheck.is(testClass, TestCase.class);
+    }
+
+    protected void gatherFiles(File classRoot, String classFileName, Hashtable result) {
+        File thisRoot = new File(classRoot, classFileName);
+        if (thisRoot.isDirectory()) {
+            String[] contents = thisRoot.list();
+            if (contents != null) {
+                for (int i = 0; i < contents.length; i++)
+                    gatherFiles(classRoot, classFileName + File.separatorChar
+                            + contents[i], result);
+            }
+        } else if (thisRoot.getName().endsWith(".class")) {
+            addToResultsIfTest(classFileName, result);
+        } else if (thisRoot.getName().endsWith(".jar")) {
+            gatherFilesInJar(thisRoot, result);
+        }
+    }
+
+    private void addToResultsIfTest(String classFileName, Hashtable result) {
+        if (isTestClass(classFileName)) {
+            String className = classNameFromFile(classFileName);
+            result.put(className, className);
+        }
+    }
+
+    private void gatherFilesInJar(File jarFile, Hashtable result) {
+        try {
+            JarFile jar = new JarFile(jarFile);
+            try {
+                Enumeration enumeration = jar.entries();
+                while (enumeration.hasMoreElements()) {
+                    JarEntry next = (JarEntry) enumeration.nextElement();
+                    if (next.getName().endsWith(".class") && next.getName().indexOf('$') == -1) {
+                        addToResultsIfTest(next.getName(), result);
+                    }
+                }
+            } finally {
+                jar.close();
+            }
+        } catch (IOException ex) {
+        }
+    }
+    //Copied from ClassPathTestCollector because we can't since they implemented private methods
+    //and we needed to override gatherFiles(File,String,Hashtable). It seems silly to make an extensible
+    //class and make some of the methods private.
+    // The rest of this class is copied from ClassPathTestCollector
+    protected String classNameFromFile(String classFileName) {
+        // convert /a/b.class to a.b
+        String s= classFileName.substring(0, classFileName.length()- SUFFIX_LENGTH);
+        String s2= s.replace(File.separatorChar, '.');
+        //changed this line for jar files (names are always / no matter the FileSystem
+        s2 = s2.replace('/', '.');
+        if (s2.startsWith("."))
+            return s2.substring(1);
+        return s2;
+    }
+    public Enumeration collectTests() {
+        String classPath= System.getProperty("java.class.path");
+        Hashtable result = collectFilesInPath(classPath);
+        return result.elements();
+    }
+
+    public Hashtable collectFilesInPath(String classPath) {
+        Hashtable result= collectFilesInRoots(splitClassPath(classPath));
+        return result;
+    }
+    
+    protected Hashtable collectFilesInRoots(Vector roots) {
+        Hashtable result= new Hashtable(100);
+        Enumeration e= roots.elements();
+        while (e.hasMoreElements()) 
+            gatherFiles(new File((String)e.nextElement()), "", result);
+        return result;
+    }
+    protected Vector splitClassPath(String classPath) {
+        Vector result= new Vector();
+        String separator= System.getProperty("path.separator");
+        StringTokenizer tokenizer= new StringTokenizer(classPath, separator);
+        while (tokenizer.hasMoreTokens()) 
+            result.addElement(tokenizer.nextToken());
+        return result;
     }
 
 }
