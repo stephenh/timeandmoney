64492bd5ed479f6afb519e15a764f9eec9e433d5
westajay <westajay@a8ba37aa-5335-0410-8f99-849fd68a1e00>
2007-04-02 04:49:58 +0000

    Added license header
    git-svn-id: https://timeandmoney.svn.sourceforge.net/svnroot/timeandmoney/timeandmoney/trunk@205 a8ba37aa-5335-0410-8f99-849fd68a1e00

--- tests/src/com/domainlanguage/money/MoneyTest.java
+++ tests/src/com/domainlanguage/money/MoneyTest.java
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) 2005 Domain Language, Inc. (http://domainlanguage.com) This
+ * free software is distributed under the "MIT" licence. See file licence.txt.
+ * For more information, see http://timeandmoney.sourceforge.net.
+ */
 package com.domainlanguage.money;
 
 import java.math.*;
