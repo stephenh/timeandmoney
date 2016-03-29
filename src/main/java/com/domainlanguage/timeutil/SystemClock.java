74f6bcd9efa70a2b8ff82c2b157e6fd40a0fa32e
Stephen Haberman <stephen.haberman@gmail.com>
2016-03-29 13:48:15 -0500

    Update SystemClock to now allow time to move backwards.

--- src/main/java/com/domainlanguage/timeutil/SystemClock.java
+++ src/main/java/com/domainlanguage/timeutil/SystemClock.java
@@ -13,6 +13,8 @@ import com.domainlanguage.time.TimeSource;
 
 public class SystemClock {
 
+  private static TimePoint nowHighPoint = TimePoint.now();
+
   public static TimeSource timeSource() {
     return new TimeSource() {
       public TimePoint now() {
@@ -21,7 +23,24 @@ public class SystemClock {
     };
   }
 
-  public static TimePoint now() {
-    return TimePoint.from(new Date());
+  public static synchronized TimePoint now() {
+    TimePoint now = TimePoint.from(new Date());
+    // We've seen clock drift backwards due to ntp adjustments in production,
+    // and it can cause obscure bugs where code will do:
+    //
+    // t1 = clock.now();
+    // ...
+    // t2 = clock.now();
+    // ...
+    // interval = t1.until(t2); // fails due to t2 being *before* t1
+    //
+    // Rather than defensively code against this in each spot that uses
+    // clock.now() and TimeIntervals, it's easier to just have SystemClock
+    // self-enforce the contract of time not moving backwards.
+    if (now.isBefore(nowHighPoint)) {
+      return nowHighPoint;
+    }
+    nowHighPoint = now;
+    return now;
   }
 }
